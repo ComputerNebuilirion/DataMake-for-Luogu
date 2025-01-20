ce584ca57750c83e776136b25baf1e89f73dbea7
Shiina-Mahiru <89201586+ComputerNebuilirion@users.noreply.github.com>
2025-01-20 15:17:42 +0800

    Delete GPT-SoVITS-v2-240821 directory

--- GPT-SoVITS-v2-240821/go-cli.bat
+++
@@ -1,2 +0,0 @@
-cd GPT-SoVITS-v2-240821
-runtime\python.exe GPT_SoVITS/inference_cli.py --gpt_model GPT_weights_v2\流萤-e10.ckpt --sovits_model SoVITS_weights_v2\流萤_e15_s810.pth --ref_audio firefly\ref_audio\example.wav --ref_text ref_text.txt --ref_language 中文 --target_text target_text.txt --target_language 中文 --output_path output
